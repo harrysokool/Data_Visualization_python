d766a6efa2c90b466fcebe543eb8e6f7fd12c687
“harrysokool” <harry1232003@icloud.com>
2024-01-03 19:45:01 -0800

    rainfall analysis

---
+++ rainfall_analysis/death_valley_rainfall.py
@@ -0,0 +1,29 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = '/Users/harrysocool/Desktop/Data_Visualization_python/data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    print(header_row)
+
+    rainfalls = []
+    dates = []
+    for row in reader:
+        date = datetime.strptime(row[2], "%Y-%m-%d")
+        rainfall = float(row[3])
+        dates.append(date)
+        rainfalls.append(rainfall)
+
+
+fig, ax = plt.subplots()
+ax.plot(dates, rainfalls)
+
+plt.title("Sitka rainfall in 2018", fontsize = 20)
+plt.xlabel("", fontsize = 16)
+fig.autofmt_xdate()
+plt.ylabel("Amount (ml)", fontsize = 16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
