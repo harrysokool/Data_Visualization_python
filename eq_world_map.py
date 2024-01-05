9eded658dab9c298abcc1056eb4193ae2c08bcc8
“harrysokool” <harry1232003@icloud.com>
2024-01-04 23:56:10 -0800

    complete earthquake graph

--- eq_world_map.py
+++ eq_world_map.py
@@ -2,7 +2,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 import json
 
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -13,15 +13,30 @@ print(len(all_eq_dicts))
 mags = []
 lons = []
 lats = []
+hovers = []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hovers.append(title)
 
-data = [Scattergeo(lon = lons, lat = lats)]
+data = [{
+    'type': 'scattergeo',
+    'lat' : lats,
+    'lon' : lons,
+    'text' : hovers,
+    'marker' : {
+        'size' : [5*mag for mag in mags],
+        'color' : mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    }
+}]
 my_layout = Layout(title = "Global Earthquakes")
 
 fig = {'data': data, 'layout': my_layout}
